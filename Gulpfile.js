697d0d3eaf80376d19a7fb9a1de25bebd0998043
Boris Okunskiy <i@savant.pro>
2015-04-06 17:24:07 +0300

    Established workflow with postcss, external variables and toolkit.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,30 @@
 "use strict";
 
-var gulp = require('gulp');
+var gulp = require('gulp')
+  , fs = require('fs-extra')
+  , Theme = require('prostore.theme-tools');
 
 gulp.task('stylesheets', function() {
-  return gulp.src(['stylesheets/**/*.css', '!stylesheets/**/_*.css'])
-    .pipe(require('gulp-postcss')([
-      require('autoprefixer')(),
-      require('postcss-import')(),
-      require('postcss-simple-vars')()
-    ]))
-    .pipe(gulp.dest('static/stylesheets'));
+  var theme = Theme(process.cwd());
+  theme.compileOptions(function(err, vars){
+    if (err) console.log(err);
+    return gulp.src(['stylesheets/**/*.css', '!stylesheets/**/_*.css'])
+      .pipe(require('gulp-postcss')([
+        require('autoprefixer')(),
+        require('postcss-import')(),
+        require('postcss-discard-comments')(),
+        require('postcss-color-function')(),
+        require('postcss-simple-vars')({
+          variables: vars
+        })
+      ]))
+      .on('error', function(err) {
+        console.error(err);
+        this.emit('end');
+      })
+      .pipe(gulp.dest('static/stylesheets'));
+  });
+
 });
 
 gulp.task('build-stylesheets', ['stylesheets'], function() {
@@ -22,6 +37,6 @@ gulp.task('build-stylesheets', ['stylesheets'], function() {
 
 gulp.task('build', ['build-stylesheets']);
 
-gulp.task('default', function() {
-  gulp.watch('stylesheets/**', ['stylesheets']);
+gulp.task('default', ['stylesheets'], function() {
+  gulp.watch(['stylesheets/**', 'variables.json'], ['stylesheets']);
 });
---
+++ README.md
@@ -0,0 +1,27 @@
+# Базовая тема ПроCтор
+
+## Установка
+
+1. Скачайте и установите [Node.js](http://nodejs.org)
+
+2. Скачайте и установите [Git](http://git-scm.com/)
+
+3. Установите инструменты для работы с проектом магазина
+
+    ```
+    npm install -g gulp prostore-cli 
+    ```
+    
+4. Клонируйте репозиторий проекта
+
+    ```
+    git clone --depth=1 https://github.com/prstr/theme.base my_store
+    cd my_store
+    ```
+
+5. Установите зависимости
+
+    ```
+    npm install
+    ```
+    
