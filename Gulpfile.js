40f590ca721e809c9d887c14d6b4be35a001913e
Boris Okunskiy <i@savant.pro>
2015-04-08 19:17:16 +0300

    Cleanup after migration to theme-api

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,41 +1,15 @@
 "use strict";
 
 var gulp = require('gulp')
-  , fs = require('fs-extra')
-  , Theme = require('prostore.theme-tools');
+  , Theme = require('prostore.theme-api');
 
-gulp.task('stylesheets', function() {
-  var theme = Theme(process.cwd());
-  theme.compileOptions(function(err, vars){
-    if (err) console.log(err);
-    return gulp.src(['stylesheets/**/*.css', '!stylesheets/**/_*.css'])
-      .pipe(require('gulp-postcss')([
-        require('autoprefixer')(),
-        require('postcss-import')(),
-        require('postcss-discard-comments')(),
-        require('postcss-color-function')(),
-        require('postcss-simple-vars')({
-          variables: vars
-        })
-      ]))
-      .on('error', function(err) {
-        console.error(err);
-        this.emit('end');
-      })
-      .pipe(gulp.dest('static/stylesheets'));
-  });
+var theme = new Theme(process.cwd());
 
+gulp.task('stylesheets', function(cb) {
+  theme.buildStylesheets(cb);
 });
 
-gulp.task('build-stylesheets', ['stylesheets'], function() {
-  return gulp.src('static/stylesheets/**/*.css')
-    .pipe(require('gulp-minify-css')({
-      keepBreaks: true
-    }))
-    .pipe(gulp.dest('static/stylesheets'));
-});
-
-gulp.task('build', ['build-stylesheets']);
+gulp.task('build', ['stylesheets']);
 
 gulp.task('default', ['stylesheets'], function() {
   gulp.watch(['stylesheets/**', 'variables.json'], ['stylesheets']);
